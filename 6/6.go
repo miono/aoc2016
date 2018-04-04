041c54b1f9869b170f575449bf7febf3d4c3c87f
Markus Nilsson <miono@users.noreply.github.com>
2018-04-04 08:45:19 +0200

    Solution to first part of day 6

---
+++ 6/6.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func getRealMsg(straangs []string) string {
+
+	var outString string
+	for i := range straangs[0] {
+		var charList []string
+		for _, x := range straangs {
+			charList = append(charList, string(x[i]))
+		}
+		charMap := make(map[string]int)
+		for _, char := range charList {
+			if _, ok := charMap[char]; ok {
+				charMap[char]++
+			} else {
+				charMap[char] = 1
+			}
+		}
+		// Get most common character
+		var highestChar string
+		var highestCount int
+		for k, v := range charMap {
+			if v > highestCount {
+				highestChar = k
+				highestCount = v
+			}
+		}
+		outString = outString + highestChar
+
+	}
+
+	return outString
+}
+
+func main() {
+	f, err := os.Open("./input")
+	if err != nil {
+		panic(err)
+	}
+	var msgs []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		msgs = append(msgs, scanner.Text())
+	}
+	fmt.Println(getRealMsg(msgs))
+}

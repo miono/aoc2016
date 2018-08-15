37728707d5d1b4a3d8dc0d12090188c729bfa018
Markus Nilsson <markus.nilsson@bonniergaming.com>
2018-08-15 23:21:00 +0200

    Complete ripoff from https://www.reddit.com/r/adventofcode/comments/5hbygy/2016_day_9_solutions/daz279z/

--- 9/9.go
+++ 9/9.go
@@ -4,13 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
+func decompress(s string, p int) int {
+	var instructions []string
+	if strings.Index(s, "(") == -1 {
+		return len(s)
+	}
+	ret := 0
+	for strings.Index(s, "(") != -1 {
+		boundStart := strings.Index(s, "(")
+		ret += boundStart
+		s = s[boundStart:]
+		boundEnd := strings.Index(s, ")")
+		instructions = strings.Split(s[1:boundEnd], "x")
+		s = s[boundEnd+1:]
+		numChars, _ := strconv.Atoi(instructions[0])
+		numRepeat, _ := strconv.Atoi(instructions[1])
+		if p == 1 {
+			ret += len(s[:numChars]) * numRepeat
+		} else if p == 2 {
+			ret += decompress(s[:numChars], 2) * numRepeat
+		}
+		s = s[numChars:]
+	}
+	ret += len(s)
+	return ret
+}
+
 func main() {
-	var outString string
 	f, err := os.Open("./input")
 	defer f.Close()
 	if err != nil {
@@ -19,34 +43,6 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	scanner.Scan()
 	fullString := scanner.Text()
-	reFind, err := regexp.Compile("\\(\\d+x\\d+\\)")
-	if err != nil {
-		panic(err)
-	}
-	reSplit, err := regexp.Compile("[)(x]")
-	if err != nil {
-		panic(err)
-	}
-	for {
-		// fmt.Println(outString)
-		bounds := reFind.FindStringIndex(fullString)
-		if bounds == nil {
-			outString = outString + fullString
-			break
-		}
-		outString = outString + fullString[:bounds[0]]
-		fmt.Println(bounds)
-		instruction := fullString[bounds[0]:bounds[1]]
-		fmt.Println(instruction)
-		charAndRepeat := reSplit.Split(instruction, -1)
-		numChar, _ := strconv.Atoi(charAndRepeat[1])
-		numRepeat, _ := strconv.Atoi(charAndRepeat[2])
-		charsToRepeat := fullString[bounds[1] : bounds[1]+numChar]
-		stringToAdd := strings.Repeat(charsToRepeat, numRepeat)
-		outString = outString + stringToAdd
-		fullString = fullString[bounds[1]+numChar:]
-
-	}
-	fmt.Println(len(outString))
-
+	fmt.Println("Solution part 1:", decompress(fullString, 1))
+	fmt.Println("Solution part 2:", decompress(fullString, 2))
 }
